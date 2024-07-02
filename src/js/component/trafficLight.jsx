918607bfdcf81551e60d42ced3272512484e76c9
carlesViolan <carlesviolan@gmail.com>
2024-07-02 13:58:58 +0000

    Carles_Violan_TrafficLight_React

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -1,61 +1,52 @@
 import React, {useState, useEffect} from "react";
 
-// create the component
 const Trafficlight = () => {
-    let [classNameRed, setclassNameRed]= useState('rounded-5');
-    let [classNameYellow, setclassNameYellow]= useState('rounded-5');
-    let [classNameGreen, setclassNameGreen]= useState('rounded-5');
+    const [classNameRed, setClassNameRed] = useState('rounded-5');
+    const [classNameYellow, setClassNameYellow] = useState('rounded-5');
+    const [classNameGreen, setClassNameGreen] = useState('rounded-5');
+    const [color, setColor] = useState("");
 
-    const [color, setcolor] = useState("");
-    console.log(color);
+    useEffect(() => {
+        switch(color) {
+            case "red":
+                setClassNameRed('bg-danger rounded-circle');
+                setClassNameYellow('rounded-circle');
+                setClassNameGreen('rounded-circle');
+                alert("Stop!!! the TrafficLight is red");
+                break;
+            case "yellow":
+                setClassNameRed('rounded-circle');
+                setClassNameYellow('bg-warning rounded-circle');
+                setClassNameGreen('rounded-circle');
+                alert("You are in a hurry, the TrafficLight is yellow");
+                break;
+            case "green":
+                setClassNameRed('rounded-circle');
+                setClassNameYellow('rounded-circle');
+                setClassNameGreen('bg-success rounded-circle');
+                alert("You can walk, the TrafficLight is green");
+                break;
+            default:
+                break;
+        }
+    }, [color]);
 
-    const prueba = () => {
-        if("red") {
-           //classNameRed +="active";
-           setclassNameRed(classNameRed ='bg-danger rounded-circle') ;
-           setclassNameYellow(classNameYellow ='rounded-circle') ;
-           setclassNameGreen(classNameGreen ='rounded-circle') ;
-           alert("Stop!!! the TrafficLight is "+classNameRed);
-        }
-          
-        };
-    const prueba2 = () => {
-        
-        if("yellow") {
-            //classNameYellow +="active";
-            setclassNameRed(classNameRed ='rounded-circle') ;
-            setclassNameYellow(classNameYellow ='bg-warning rounded-circle') ;
-            setclassNameGreen(classNameGreen ='rounded-circle') ;
-            alert("You are in a hurry, the TrafficLight is "+classNameYellow);
-        }
-        };
-    const prueba3 = () => {
-        if("green"){
-            //classNameGreen +="active";
-            setclassNameRed(classNameRed ='rounded-circle') ;
-            setclassNameGreen(classNameGreen ='bg-success rounded-circle') ;
-            setclassNameYellow(classNameYellow ='rounded-circle') ;
-            alert("You can walk, the TrafficLight is "+classNameGreen);
-          
-        }
-
-    };
+    const handleRedClick = () => setColor("red");
+    const handleYellowClick = () => setColor("yellow");
+    const handleGreenClick = () => setColor("green");
 
     return (
         <div className="text-center">
             <h1>Traffic Light</h1>
             <div className="semafor">
-                <span id="redLight" onClick={() => prueba()} className={classNameRed}></span>
+                <span id="redLight" onClick={handleRedClick} className={classNameRed}></span>
                 <br />
-                <span id="yellowLight" onClick={() => prueba2()} className={classNameYellow}></span>
+                <span id="yellowLight" onClick={handleYellowClick} className={classNameYellow}></span>
                 <br />
-                <span id="greenLight" onClick={() => prueba3()} className={classNameGreen}></span>
+                <span id="greenLight" onClick={handleGreenClick} className={classNameGreen}></span>
             </div>
         </div>
     );
-
-    
-   
 };
 
 export default Trafficlight;
